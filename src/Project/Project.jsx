ba5a72097ba3b4e2ddabf8b8ac204401c0c7b8ca
sakeel <sakeelnoufas7@gmail.com>
2026-03-19 15:34:52 +0530

    ready for deployment

--- src/Project/Project.jsx
+++ src/Project/Project.jsx
@@ -40,7 +40,7 @@ const Project = () => {
       <div className="cert-card">
         <h3>MERN Stack Course</h3>
         <p className="cert-description">Completed MERN Stack training at Qtree Technologies, gaining hands-on experience in MongoDB, Express.js, React, and Node.js.
-Built full-stack web applications with frontend, backend, and database integration using JavaScript.</p>
+          Built full-stack web applications with frontend, backend, and database integration using JavaScript.</p>
         <button
           className="project-btn"
           onClick={() => setActiveCert(`${import.meta.env.BASE_URL}certificates/mern-cert.pdf`)}
@@ -55,7 +55,7 @@ Built full-stack web applications with frontend, backend, and database integrati
       <div className="cert-card">
         <h3>Web Development Intern</h3>
         <p className="cert-description">Completed internship at Cubiksoft Technologies Pvt. Ltd., working on a MERN stack-based food ordering web application.
-Demonstrated strong problem-solving skills, dedication, and ability to build full-stack projects using modern web technologies.</p>
+        Demonstrated strong problem-solving skills, dedication, and ability to build full-stack projects using modern web technologies.</p>
         <button
           className="project-btn"
           onClick={() => setActiveCert(`${import.meta.env.BASE_URL}certificates/internship-cert.pdf`)}
